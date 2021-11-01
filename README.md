4bfee63f3c5ea2fb7f1d965e9f9cec8b31766ae1
moritz155 <m.schelten@gmx.de>
2021-11-01 19:20:48 +0100

    update logic and create one data interface instead of one per view

--- README.md
+++ README.md
@@ -1,10 +1,14 @@
-# Bierboerse
+# Bierbörse
 
-## Architecture: 
-### Components: 
+## Components: 
 + frontends (currently two)
 + a view function for the frontends to get the data 
 + calculator function 
     + calculates the current prices (and several other stuff) for each drink
 + backend 
+
+## Interesting facts
++ the views update their data in an interval of `normal_interval` / 5
+  + the `normal_interval` is the interval in which new data is calculated
+  + by requesting new data five times more often than there actually is new data the time in which the views have different kinds of data is minimized
 
--- routes.py
+++ routes.py
@@ -62,7 +62,7 @@ def update_data():  # data returns the last eight prices only
         mult = 0
         list = []
         for price in drink.price_history:
-            list.append((start_time + 30 * mult, round(price, 2)))
+            list.append((start_time + 30 * mult, str(price)[0:3]))
             mult = mult + 1
         if len(list) > 8:
             data[drink.name] = list[-8:]
@@ -120,17 +120,13 @@ def fullScreen():
 
 @app.route('/table')
 def table():  # table needs: name, price, price difference, max, min
-    table_data = build_data_for_table()
+    table_data = calculator()
     return render_template('beerTable.html', beers=table_data, colorSet=customColorSet, iteration=iteration_interval)
 
 
 @app.route('/data/table')
 def data_for_table():  # table needs: name, price, price difference, max, min
-    if time.time() - updated_last_time >= iteration_interval*2:
-        table_data = build_data_for_table()
-        return json.dumps(table_data)
-    print("aaiiaiai")
-    return old_table_data
+    return calculator()
 
 
 @app.route('/')
@@ -148,68 +144,80 @@ def ordered_Drink():  # receives the orders and adds it to the drink objects
     return ""
 
 
-# different views of this flask app will update at different times depending on the time they started
-# In case we want more views(table and chart) this must be fixed!
-@app.route('/data/chart')
-def prices():
-    global current_time
-    result = {}
-    timestamp = request.args.get('timestamp')
-    if timestamp is not None:
-        timestamp = float(timestamp)
-    if time.time() - current_time > iteration_interval:  # ensure that prices never update more often than interval
-        current_time = time.time()
-        simulation()
-        newly_bought = 0
-        for drink in allDrinks:
-            drink.newOrders()
-            newly_bought = newly_bought + drink.newCounter
-            # print(drink.name + str(drink.newCounter))
-        counter = 0
-        changePrice = []  # takes drink if price was added in this period
-        for drink in allDrinks:
-            relative_part = 0.0
-            if not newly_bought == 0:
-                relative_part = drink.newCounter / newly_bought
-            if relative_part >= 0.25:  # relative part must be higher than 25% to increase price
-                drink.addNewRandomPrice(True, True)
+def calculator():  # calculates the new prices and returns a json with all necessary datapoints
+    # create json object with following attributes
+    # name, price, price difference, max, min
+    global current_time, updated_last_time, old_table_data
+
+    if time.time() - updated_last_time >= iteration_interval:
+        updated_last_time = time.time()
+        all_drinks_data = {}
+        result = {}
+        timestamp = request.args.get('timestamp')
+        if timestamp is not None:
+            timestamp = float(timestamp)
+        if time.time() - current_time > iteration_interval:  # ensure that prices never update more often than interval
+            current_time = time.time()
+            simulation()
+            newly_bought = 0
+            for drink in allDrinks:
+                drink.newOrders()
+                newly_bought = newly_bought + drink.newCounter
+                # print(drink.name + str(drink.newCounter))
+            counter = 0
+            changePrice = []  # takes drink if price was added in this period
+            table_data = []
+            # snd part of logic
+            for drink in allDrinks:
+                relative_part = 0.0
+                if not newly_bought == 0:
+                    relative_part = drink.newCounter / newly_bought
+                if relative_part >= 0.25:  # relative part must be higher than 25% to increase price
+                    drink.addNewRandomPrice(True, True)
+                    changePrice.append(drink)
+                    continue
+                elif relative_part == 0 and counter == 0:  # decrease price of first drink that wasnt bought in this period
+                    counter = 1  # secures that only one price will decrease
+                    drink.addNewRandomPrice(False, True)
+                    changePrice.append(drink)
+                    continue
+            if len(recentlyChangedPrices) == len(allDrinks):
+                recentlyChangedPrices[0].addNewRandomPrice(False, True)
+                changePrice.append(recentlyChangedPrices[-1])
+            else:
+                notChangedYet = [x for x in allDrinks if x not in recentlyChangedPrices]
+                drink = random.choice(notChangedYet)
+                drink.addNewRandomPrice(False, True)  # takes random drink of those without price change_price
                 changePrice.append(drink)
-                continue
-            elif relative_part == 0 and counter == 0:  # decrease price of first drink that wasnt bought in this period
-                counter = 1  # secures that only one price will decrease
-                drink.addNewRandomPrice(False, True)
-                changePrice.append(drink)
-                continue
-        if len(recentlyChangedPrices) == len(allDrinks):
-            recentlyChangedPrices[0].addNewRandomPrice(False, True)
-            changePrice.append(recentlyChangedPrices[-1])
-        else:
-            notChangedYet = [x for x in allDrinks if x not in recentlyChangedPrices]
-            drink = random.choice(notChangedYet)
-            drink.addNewRandomPrice(False, True)  # takes random drink of those without price change_price
-            changePrice.append(drink)
-        addPriceTo = [x for x in allDrinks if
-                      x not in changePrice]  # includes all drinks that do not have enough prices
-        for drink in addPriceTo:
-            drink.addNewRandomPrice(True, False)  # all drinks now have same number of prices
-        update_data()
-        #         global clock_for_analysis
-        #         # if time.time() - clock_for_analysis >= 140:
-        #         #     analysis()
-        #         #     clock_for_analysis = float(time.time())
-    for drink in data:
-        result[drink] = []
-        for i in data[drink]:
-            if timestamp is None or i[1] >= timestamp:
-                result[drink].append(i)
-    return json.dumps(result)
+            addPriceTo = [x for x in allDrinks if
+                          x not in changePrice]  # includes all drinks that do not have enough prices
+            for drink in addPriceTo:
+                drink.addNewRandomPrice(True, False)  # all drinks now have same number of prices
+            update_data()
+        for drink in allDrinks:
+            # part for table data
+            single_drink_dict = {"name": drink.name, "price": str(drink.price_history[-1])[0:3]}
+            try:
+                single_drink_dict["price_diff"] = str(drink.price_history[-1] - drink.price_history[-2])[0:4]
+            except IndexError:  # if price history has only one price
+                single_drink_dict["price_diff"] = 0
+            single_drink_dict["min"] = str(drink.minPrice)[0:3]
+            single_drink_dict["max"] = str(drink.maxPrice)[0:3]
+            history_list = []
+            for i in data[drink.name]:
+                if timestamp is None or i[1] >= timestamp:
+                    history_list.append(i)
+            single_drink_dict["history"] = history_list
+            all_drinks_data[drink.name] = single_drink_dict
+        old_table_data = all_drinks_data
+    return old_table_data
 
 
 old_table_data = []
 updated_last_time = time.time()
 current_time: float = time.time()
 start_time = float(time.time())
-iteration_interval = 10  # in seconds
+iteration_interval = 5  # in seconds
 data = {}
 customColorSet = ["#FF0000",
                   "#FF8F00",
